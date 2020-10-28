b8066f5396a0bb037cedf3a1dd76a166a4bbef25
limonadev <limonadev@whiteleaf.dev>
2020-10-28 15:51:42 -0500

    Removed unnecessary loop on input

--- InvertedIndex.java
+++ InvertedIndex.java
@@ -65,10 +65,8 @@ public class InvertedIndex {
     job.setReducerClass(SaveReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
-    for (int i = 0; i < otherArgs.length - 1; ++i) {
-      FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-    }
-    FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
 
   }
